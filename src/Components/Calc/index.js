b2070d561b271b3e2c9a9ca1938d98088da337da
Vanessa Kienzl <vanessa.kienzl@a1.at>
2020-01-17 00:14:29 +0100

    bugfix

--- src/Components/Calc/index.js
+++ src/Components/Calc/index.js
@@ -34,24 +34,25 @@ class Calc extends React.Component{
         if (calc.length === 1){
             let output = calc/100
             this.props.display(output);
-            } else {
-            let index = calc.length-2; //index so the last 2 el of arr can be split off (Op + percent)
-            let x1 = calc.splice(0,index+1) // arr with el for eval for everything before the op+percent
-            let l = x1.length
-            let op = x1[l-1] //operator for final eval
-            x1 = x1.splice(0, l-1) // cut off operator
-            let pValue = calc.splice(0,index) // value of percent
-        
-            let y = x1.join('');
-            let z = pValue.join('');
-
-            let x = eval(y); // eval for calculating everything befor the op and percent
-            let percent = eval(`${x}/100*${pValue}`) // eval the percent
-            let finalResult = eval(`${x}${op}${percent}`); // combining calc and percent into
-        
-            this.props.display(finalResult);
             this.display= ' ';
-            }
+            } else {
+                let index = calc.length-2; //index so the last 2 el of arr can be split off (Op + percent)
+                let x1 = calc.splice(0,index+1) // arr with el for eval for everything before the op+percent
+                let l = x1.length
+                let op = x1[l-1] //operator for final eval
+                x1 = x1.splice(0, l-1) // cut off operator
+                let pValue = calc.splice(0,index) // value of percent
+            
+                let y = x1.join('');
+                let z = pValue.join('');
+
+                let x = eval(y); // eval for calculating everything befor the op and percent
+                let percent = eval(`${x}/100*${pValue}`) // eval the percent
+                let finalResult = eval(`${x}${op}${percent}`); // combining calc and percent into
+            
+                this.props.display(finalResult);
+                this.display= ' ';
+                }
 
         /* console.log('calc',calc);
         console.log('index',index);

871e593312db1d9f3a9c4105da928ae0542b7be0
Vanessa Kienzl <vanessa.kienzl@a1.at>
2020-01-17 00:11:40 +0100

    added if/else to percent calc open task-> switch algebraic sign

--- src/Components/Calc/index.js
+++ src/Components/Calc/index.js
@@ -11,14 +11,9 @@ class Calc extends React.Component{
     handleClickDisplay = (e) => {
         let input = e.target.value;
         this.percent = input;
-        console.log('display', typeof(this.display))
+        //console.log('display', typeof(this.display))
 
-        this.display += input;
-        console.log('display after +input', typeof(this.display))
-        
-        console.log('input', typeof(input))
-        
-
+        this.display += input;  
         this.props.display(this.display);
     }
 
@@ -35,22 +30,28 @@ class Calc extends React.Component{
 
     handlePercent = () => {
         let calc = this.display.split(' '); //splitting string in arr
-        let index = calc.length-2; //index so the last 2 el of arr can be split off (Op + percent)
-        let x1 = calc.splice(0,index+1) // arr with el for eval for everything before the op+percent
-        let l = x1.length
-        let op = x1[l-1] //operator for final eval
-        x1 = x1.splice(0, l-1) // cut off operator
-        let pValue = calc.splice(0,index) // value of percent
-       
-        let y = x1.join('');
-        let z = pValue.join('');
 
-        let x = eval(y); // eval for calculating everything befor the op and percent
-        let percent = eval(`${x}/100*${pValue}`) // eval the percent
-        let finalResult = eval(`${x}${op}${percent}`); // combining calc and percent into
-      
-        this.props.display(finalResult);
-        this.display= ' ';
+        if (calc.length === 1){
+            let output = calc/100
+            this.props.display(output);
+            } else {
+            let index = calc.length-2; //index so the last 2 el of arr can be split off (Op + percent)
+            let x1 = calc.splice(0,index+1) // arr with el for eval for everything before the op+percent
+            let l = x1.length
+            let op = x1[l-1] //operator for final eval
+            x1 = x1.splice(0, l-1) // cut off operator
+            let pValue = calc.splice(0,index) // value of percent
+        
+            let y = x1.join('');
+            let z = pValue.join('');
+
+            let x = eval(y); // eval for calculating everything befor the op and percent
+            let percent = eval(`${x}/100*${pValue}`) // eval the percent
+            let finalResult = eval(`${x}${op}${percent}`); // combining calc and percent into
+        
+            this.props.display(finalResult);
+            this.display= ' ';
+            }
 
         /* console.log('calc',calc);
         console.log('index',index);
@@ -71,6 +72,9 @@ class Calc extends React.Component{
         return(
             <div className="input container">
                 <div className="row">
+                    {console.log('lets see', this.display.length)}
+                   
+
                     <button  onClick={this.handleClear} className="col-3 clear btn btn-dark rounded-0" value='clear'>C</button>
                     <button  className="col-3 transform btn btn-dark rounded-0" value='transform'>+/-</button>
                     <button  onClick={this.handlePercent} className="col-3 percent btn btn-dark rounded-0" value="percent">%</button>
